0b9c3f3fafbcad50c8a6633a538256a5ad1e0142
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2021-06-11 12:16:52 +0800

    Update train.py
    Included new tool accuracy and other fixes

--- tools.py
+++ tools.py
@@ -1,14 +1,19 @@
-from numpy.random import shuffle
-
-def train_val_test_split(num_total, split_train=0.6, split_val=0.2):
-  idx_list = [i for i in range(num_total)]
-  shuffle(idx_list)
-
-  num_train = int(split_train * num_total)
-  num_val = int((split_train + split_val) * num_total)
-
-  idx_train = idx_list[:num_train]
-  idx_val = idx_list[num_train:num_val]
-  idx_test = idx_list[num_val]
-
-  return idx_train, idx_val, idx_test
+def accuracy(output, labels):
+    correct_false = 0
+    correct_true = 0
+    total_false = 0
+    total_true = 0
+    for o, l in zip(output, labels):
+        if l == 0:
+            total_false += 1
+        if o == 0:
+            correct_false += 1
+        elif l == 1:
+            total_true += 1
+        if o == 1:
+            correct_true += 1
+    if total_true == 0:
+        total_true = 1
+    if total_false == 0:
+        total_false = 1
+    return (correct_true + correct_false)/(total_true + total_false), correct_true/total_true, total_true, correct_false/total_false, total_false
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 
-from Generator.GraphGenerator import GraphGenerator
+from DataLoader.GraphLoader import GraphLoader
 
 import time
 import argparse
@@ -15,7 +15,60 @@ import argparse
 
 from tqdm import tqdm
 
-from Model import GCN, FocalLoss
+from Model.GCN import GCN
+from tools import accuracy
+
+def train(epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    graph_loader.mode = 'train'
+    total_loss_train = 0
+    output_list = []
+    labels_list = []
+
+    for i, data in enumerate(tqdm(graph_loader, total=len(graph_loader))):
+        adj, feat, label = data
+        if args.cuda:
+            adj = adj.cuda()
+            feat = feat.cuda()
+            label = label.cuda()
+
+        if i % args.batch_size == 0:
+            current = model.forward(feat, adj)
+            output = current.unsqueeze(0)
+            labels = label
+        else:
+            current = model.forward(feat, adj)
+            output = torch.vstack((output, current.unsqueeze(0)))
+            labels = torch.vstack((labels, label))
+
+        output_list.append(int(torch.argmax(current)))
+        labels_list.append(int(label))
+
+        if i % args.batch_size == args.batch_size - 1:
+            labels = torch.flatten(labels)
+            loss_train = F.cross_entropy(
+                output, labels, weight=weights)
+            total_loss_train += float(loss_train)
+            # acc_train = accuracy(output, labels)
+            loss_train.backward()
+            optimizer.step()
+
+    f1_train = f1_score(labels_list, output_list)
+    acc_train = accuracy(output_list, labels_list)
+    # model.eval()
+    
+    print('\nEpoch: {:04d}'.format(epoch+1),
+        # 'loss_train: {:.4f}'.format(loss_train.item()),
+        'total_loss_train: {:.4f}'.format(total_loss_train),
+        'f1_train: {:.4f}'.format(f1_train),
+        'acc_train: {:.4f} ({.4f} || {.4f})'.format(acc_train[0], acc_train[1], acc_train[2]),
+        # 'loss_val: {:.4f}'.format(loss_val.item()),
+        # 'avg_loss_val: {:.4f}'.format(total_loss_val/len(idx_val)),
+        # 'f1_val: {:.4f}'.format(f1_val),
+        # 'acc_val: {:.4f}'.format(acc_val.item()),
+        'time: {:.4f}s'.format(time.time() - t))
 
 if __name__ == "__main__":
 
@@ -25,6 +78,7 @@ if __name__ == "__main__":
     parser.add_argument('--seed', type=int, default=200,
                         help='Seed for random functions (default: 200')
     parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--dropout', type=float, default=0.1)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
     parser.add_argument('--cuda', action='store_true', default=True,
@@ -35,53 +89,23 @@ if __name__ == "__main__":
     args.cuda = args.cuda and torch.cuda.is_available()
 
     np.random.seed(args.seed)
-    graph_generator = GraphGenerator(args.category)
+    graph_loader = GraphLoader(args.category)
 
     torch.manual_seed(args.seed)
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
 
-    model = GCN.GCN(nfeat=4096,
+    model = GCN(nfeat=4096,
                 nclass=2,
                 dropout=args.dropout)
 
     optimizer = optim.Adam(model.parameters(),
-                           lr=args.lr, weight_decay=args.weight_decay)
-
+                        lr=args.lr, weight_decay=args.weight_decay)
+    weights = torch.Tensor([1, 30])
     if args.cuda:
         model.cuda()
+        weights.cuda()
 
     for epoch in range(args.epochs):
-        t = time.time()
-        model.train()
-        optimizer.zero_grad()
-        graph_generator.mode = 'train'
-        total_loss_train = 0
-
-        for feat, adj, labels in graph_generator:
-            output = model(feat, adj)
-            loss_train = F.cross_entropy(
-                output, labels, weight=weights)
-            total_loss_train += float(loss_train)
-            # acc_train = accuracy(output, labels)
-            loss_train.backward()
-            optimizer.step()
-        
-            f1_train = f1_score(labels.cpu(), output)
-
-        # model.eval()
-        
-
-        print('\nEpoch: {:04d}'.format(epoch+1),
-            # 'loss_train: {:.4f}'.format(loss_train.item()),
-            'avg_loss_train: {:.4f}'.format(total_loss_train/len(idx_train)),
-            'f1_train: {:.4f}'.format(f1_train),
-            # 'acc_train: {:.4f}'.format(acc_train.item()),
-            # 'loss_val: {:.4f}'.format(loss_val.item()),
-            # 'avg_loss_val: {:.4f}'.format(total_loss_val/len(idx_val)),
-            # 'f1_val: {:.4f}'.format(f1_val),
-            # 'acc_val: {:.4f}'.format(acc_val.item()),
-            'time: {:.4f}s'.format(time.time() - t))
-
-        # return loss_train, acc_train, loss_val, acc_val
-
+        train(epoch)
+        torch.save(model.state_dict(), 'ckpt_{}.pth'.format(epoch))
